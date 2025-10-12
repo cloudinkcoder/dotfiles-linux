4b320cb69fe9af95e48b8feec5109b2e90547191
Aaron Chan <cs.yelling123@passinbox.com>
2025-10-12 19:47:24 +0800

    update

--- texlive/install.sh
+++ texlive/install.sh
@@ -8,8 +8,11 @@ tar -xzf install-tl-unx.tar.gz && rm -f install-tl-unx.tar.gz
 ./install-tl-*/install-tl --profile=texlive.profile --repository=https://mirrors.aliyun.com/CTAN/systems/texlive/tlnet
 rm -rf install-tl-*
 
-# 设置镜像源
-tlmgr option repository https://mirrors.aliyun.com/CTAN/systems/texlive/tlnet
+# 永久设置镜像源 (阿里镜像为例子)
+# tlmgr option repository https://mirrors.aliyun.com/CTAN/systems/texlive/tlnet
+# 根据地理位置自动选择镜像源 (推荐)
+tlmgr option repository https://mirror.ctan.org/systems/texlive/tlnet
+
 # 设置保留最近一次备份 (默认设置)
 # tlmgr option autobackup 1
 # 额外安装字体（英文和中文）
